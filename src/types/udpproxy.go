ae893c85ac9cc6611e0860e404ec693f6bb100d2
DrewCyber <DruCyber@gmail.com>
2025-12-09 14:50:00 +0400

    Untested. Fix wireguard connect via UDP-forwarded port

--- src/types/udpproxy.go
+++ src/types/udpproxy.go
@@ -4,7 +4,7 @@ import (
 	"net"
 )
 
-func ReverseProxyUDP(mtu uint64, dst net.PacketConn, dstAddr net.Addr, src net.Conn) error {
+func udpProxyFunc(mtu uint64, dst net.PacketConn, dstAddr net.Addr, src net.Conn) error {
 	buf := make([]byte, mtu)
 	for {
 		n, err := src.Read(buf[:])
@@ -18,5 +18,45 @@ func ReverseProxyUDP(mtu uint64, dst net.PacketConn, dstAddr net.Addr, src net.C
 			}
 		}
 	}
-	return nil
+}
+
+func ReverseProxyUDP(mtu uint64, dst net.PacketConn, dstAddr net.Addr, src net.Conn) error {
+	// Start bidirectional proxying
+	errCh := make(chan error, 2)
+
+	// Forward: src -> dst (with dstAddr)
+	go func() {
+		errCh <- udpProxyFunc(mtu, dst, dstAddr, src)
+	}()
+
+	// Backward: dst -> src (read from dst, write to src)
+	go func() {
+		buf := make([]byte, mtu)
+		for {
+			n, addr, err := dst.ReadFrom(buf[:])
+			if err != nil {
+				errCh <- err
+				return
+			}
+			// Only forward packets from the expected address
+			if addr.String() == dstAddr.String() {
+				if n > 0 {
+					_, err = src.Write(buf[:n])
+					if err != nil {
+						errCh <- err
+						return
+					}
+				}
+			}
+		}
+	}()
+
+	// Wait for first error from either direction
+	err := <-errCh
+
+	// Close connections
+	dst.Close()
+	src.Close()
+
+	return err
 }
